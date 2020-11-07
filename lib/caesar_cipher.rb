75ba604f7f74bc6cbcb9c17e6b00d56dcaad3204
Stéphane Meny <smeny@users.noreply.github.com>
2020-11-07 18:41:10 +0000

    rubocop corrections

--- lib/caesar_cipher.rb
+++ lib/caesar_cipher.rb
@@ -1,19 +1,19 @@
+# frozen_string_literal: true
+
 class CaesarCipher
-    ALPHABET_WRAP = 26.freeze
+  ALPHABET_WRAP = 26
 
-    def caesar_cipher(clear_text, shift)
-        result = ""
-        clear_text.each_char do |character|
-            result << shift_within_alphabet(character, shift)
-        end
-        return result
+  def caesar_cipher(clear_text, shift)
+    result = ''
+    clear_text.each_char do |character|
+      result << shift_within_alphabet(character, shift)
     end
+    result
+  end
 
-    def shift_within_alphabet(character, shift)
-        shifted_ordinal = character.ord + shift
-        if (shifted_ordinal > "z".ord)
-            shifted_ordinal -= ALPHABET_WRAP
-        end
-        return shifted_ordinal.chr
-    end
+  def shift_within_alphabet(character, shift)
+    shifted_ordinal = character.ord + shift
+    shifted_ordinal -= ALPHABET_WRAP if shifted_ordinal > 'z'.ord
+    shifted_ordinal.chr
+  end
 end
--- spec/caesar_cipher_spec.rb
+++ spec/caesar_cipher_spec.rb
@@ -1,22 +1,23 @@
+# frozen_string_literal: true
+
 require 'caesar_cipher'
 
-RSpec.describe "#caesar_cipher" do
-    let(:caesar_cipher) { CaesarCipher.new }
+RSpec.describe '#caesar_cipher' do
+  let(:caesar_cipher) { CaesarCipher.new }
 
-    it "shifts a letter by one" do
-        expect(caesar_cipher.caesar_cipher("a" , 1)).to eq("b")
-    end
+  it 'shifts a letter by one' do
+    expect(caesar_cipher.caesar_cipher('a', 1)).to eq('b')
+  end
 
-    it "shifts a letter by five" do
-        expect(caesar_cipher.caesar_cipher("a" , 5)).to eq("f")
-    end
+  it 'shifts a letter by five' do
+    expect(caesar_cipher.caesar_cipher('a', 5)).to eq('f')
+  end
 
-    it "keeps case after shift" do
-        expect(caesar_cipher.caesar_cipher("A" , 1)).to eq("B")
-    end
+  it 'keeps case after shift' do
+    expect(caesar_cipher.caesar_cipher('A', 1)).to eq('B')
+  end
 
-    it "wraps after lowercase letter z" do
-        expect(caesar_cipher.caesar_cipher("z" , 3)).to eq("c")
-    end
+  it 'wraps after lowercase letter z' do
+    expect(caesar_cipher.caesar_cipher('z', 3)).to eq('c')
+  end
 end
-
